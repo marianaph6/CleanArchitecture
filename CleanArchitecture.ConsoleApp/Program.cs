4a50d6c58eed22492a0ab0fa06a7d02e90eb92d6
Mariana Palacios Hinestroza <mpalacios@sistecredito.com>
2024-01-22 10:48:05 -0500

    Insercion de record en EF

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CleanArchitecture.Data;
+using CleanArchitecture.Domain;
+
+StreamerDbContext dbContext = new();
+
+Streamer streamer = new()
+{
+    Nombre= "Amazon Prime",
+    Url = "https://www.amazonprime.com"
+};
+
+dbContext!.Streamers!.Add(streamer);
+await dbContext.SaveChangesAsync();
+
+List<Video> movies = new List<Video>()
+{
+    new Video()
+    {
+        Nombre = "Mad Max",
+        StreamerId = streamer.Id,
+
+    },
+    new Video()
+    {
+        Nombre = "Batman",
+        StreamerId = streamer.Id,
+    },
+    new Video()
+    {
+        Nombre= "SuperMan",
+        StreamerId = streamer.Id,
+    }
+};
+
+await dbContext.AddRangeAsync(movies);
+
+await dbContext.SaveChangesAsync();
