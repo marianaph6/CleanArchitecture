f8301e2a2e55fb526d3f98d0ed2cbc9faea2ad84
marianaph6 <mariana1996.mph@gmail.com>
2024-01-22 09:40:43 -0500

    Cadena de conexión DbContext

---
+++ CleanArchitecture.Domain/Streamer.cs
@@ -0,0 +1,23 @@
+﻿namespace CleanArchitecture.Domain
+{
+    /// <summary>
+    /// Streamer Class
+    /// </summary>
+    public class Streamer
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombre
+        /// </summary>
+        public string? Nombre { get; set; } //= string.Empty;
+
+        /// <summary>
+        /// Url
+        /// </summary>
+        public string? Url { get; set; }
+    }
+}
