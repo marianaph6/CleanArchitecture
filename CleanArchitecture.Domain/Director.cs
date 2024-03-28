133f3da2ae381f627429303d01bc5049513fb0cc
marianaph6 <mariana1996.mph@gmail.com>
2024-03-28 18:52:59 -0500

    Defnición de relaciones de uno a uno

--- CleanArchitecture.Domain/Director.cs
+++ CleanArchitecture.Domain/Director.cs
@@ -11,11 +11,22 @@ namespace CleanArchitecture.Domain
         /// <summary>
         /// Nombre
         /// </summary>
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
 
         /// <summary>
         /// Apellido
         /// </summary>
-        public string Apellido { get; set; }
+        public string? Apellido { get; set; }
+
+
+        /// <summary>
+        /// VideoId --> Referencia hacia la clase Video
+        /// </summary>
+        public int VideoId { get; set; }
+
+        /// <summary>
+        /// Video --> Definición de relación de uno a uno entre Director y Video
+        /// </summary>
+        public virtual Video? Video { get; set; }
     }
 }
