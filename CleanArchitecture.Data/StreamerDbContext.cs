ff4aaa48ffb00ea9d9b67064b798d6e71ad6fe09
marianaph6 <mariana1996.mph@gmail.com>
2024-03-30 21:56:09 -0500

    Consultas dinamicas entre varias tablas

--- CleanArchitecture.Data/StreamerDbContext.cs
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -34,5 +34,7 @@ namespace CleanArchitecture.Data
         public DbSet<Streamer>? Streamers { get; set; }
 
         public DbSet<Video>? Videos { get; set; }
+
+        public DbSet<Actor>? Actores { get; set; }
     }
 }
