076a9611cd6432800844f5d1497aea7250ec122c
Mariana Palacios Hinestroza <mpalacios@sistecredito.com>
2024-03-30 23:17:02 -0500

    Configuración del proyecto para que cumpla con DDD

--- CleanArchitecture.Domain/Common/BaseDomainModel.cs
+++ CleanArchitecture.Domain/Common/BaseDomainModel.cs
@@ -4,11 +4,31 @@ namespace CleanArchitecture.Domain.Common
     /// <summary>
     /// BaseDomainModel Class
     /// </summary>
-    public class BaseDomainModel
+    public abstract class BaseDomainModel
     {
         /// <summary>
         /// Id
         /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// DateCreated
+        /// </summary>
+        public DateTime? DateCreated { get; set; }
+
+        /// <summary>
+        /// CreatedBy
+        /// </summary>
+        public string? CreatedBy { get; set; }
+
+        /// <summary>
+        /// LastModifiedDate
+        /// </summary>
+        public DateTime? LastModifiedDate { get; set; }
+
+        /// <summary>
+        /// LastModifiedBy
+        /// </summary>
+        public string? LastModifiedBy { get; set; }
     }
 }
