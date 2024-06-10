3d2b48ca3d22ba67ec0872c46570166ec740b5d2
marianaph6 <mariana1996.mph@gmail.com>
2024-06-10 16:25:14 -0500

    Implementación de pipelines (Behaviours)

---
+++ src/Core/CleanArchitecture.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,40 @@
+﻿
+
+using FluentValidation;
+using MediatR;
+using ValidationException = CleanArchitecture.Application.Exceptions.ValidationException;
+
+namespace CleanArchitecture.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                // Buscar y ejecutar todas las validaciones de la aplicación --> Las ejecuta en el pipeline antes de que lleguen a la aplicación
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+                // Validar si hay algun error con las validaciones
+
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+    }
+}
