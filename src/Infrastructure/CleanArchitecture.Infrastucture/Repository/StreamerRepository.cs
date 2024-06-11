f156bf35e9f97e603ca0ad4d6c956731c9578472
marianaph6 <mariana1996.mph@gmail.com>
2024-06-10 20:32:00 -0500

    Implementación de repository de video y streamer

---
+++ src/Infrastructure/CleanArchitecture.Infrastucture/Repository/StreamerRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastucture.Persistence;
+
+namespace CleanArchitecture.Infrastucture.Repository
+{
+    public class StreamerRepository: RepositoryBase<Streamer>, IStreamerRepository
+    {
+        public StreamerRepository(StreamerDbContext context): base(context)
+        {
+ 
+        }
+    }
+}
