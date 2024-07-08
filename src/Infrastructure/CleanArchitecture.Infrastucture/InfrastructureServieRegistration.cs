f9c5374f89565457d280d4e18893966889507077
marianaph6 <mariana1996.mph@gmail.com>
2024-07-08 09:26:22 -0500

    Implementación de funcionalidad para el envio de correo electronico

---
+++ src/Infrastructure/CleanArchitecture.Infrastucture/InfrastructureServieRegistration.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Infrastucture.Persistence;
+using CleanArchitecture.Infrastucture.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastucture
+{
+    public static class InfrastructureServieRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<StreamerDbContext>(option => option.UseSqlServer(configuration.GetConnectionString("ConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IVideoRepository, VideoRepository>();
+            services.AddScoped<IStreamerRepository, StreamerRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, IEmailService>();
+
+
+            return services;
+
+
+        }
+    }
+}
