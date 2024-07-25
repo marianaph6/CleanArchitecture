4f4a29f2e36b1bd000f98981d1aa8fa38a51d992
marianaph6 <mariana1996.mph@gmail.com>
2024-07-25 14:09:27 -0500

    Base de datos InMemory EF y UnitTest

---
+++ test/CleanArchitecture.Application.UnitTests/Mocks/MockUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using Moq;
+
+namespace CleanArchitecture.Application.UnitTests.Mocks
+{
+    //Objetivo → Representar/Administarr las instancias de los repositorios (Directors,Videos,Streamer)
+    public static class MockUnitOfWork
+    {
+        public static Mock<IUnitOfWork> GetUnitOfWork()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var mockVideoRepository = MockVideoRepository.GetVideoRepository();
+            mockUnitOfWork.Setup(r => r.VideoRepository).Returns(mockVideoRepository.Object);
+            return mockUnitOfWork;
+
+        }
+    }
+
+}
---
+++ test/CleanArchitecture.Application.UnitTests/Mocks/MockVideoRepository.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture;
+using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastucture.Persistence;
+using CleanArchitecture.Infrastucture.Repository;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace CleanArchitecture.Application.UnitTests.Mocks
+{
+    public static class MockVideoRepository
+    {
+        public static Mock<VideoRepository> GetVideoRepository()
+        {
+            // Crear data de prueba con la libreria Fixture
+            var fixture = new Fixture();
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            var videos = fixture.CreateMany<Video>().ToList();
+            videos.Add(fixture.Build<Video>()
+                .With(tr => tr.CreatedBy, "System")
+                .Create()
+                );
+
+            var options = new DbContextOptionsBuilder<StreamerDbContext>()
+                .UseInMemoryDatabase(databaseName: $"StreamerDbContext-{Guid.NewGuid}"
+                ).Options;
+
+            var streamerDbContextFake = new StreamerDbContext(options);
+            streamerDbContextFake.Videos!.AddRange(videos);
+            streamerDbContextFake.SaveChanges();
+
+            var mockRepository = new Mock<VideoRepository>(streamerDbContextFake);
+            return mockRepository;
+        }
+    }
+
+}
